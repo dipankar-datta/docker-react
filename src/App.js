0208bbfc7cb2e6280da2a50fb13c67fb1246f64f
root <dattapro5@gmail.com>
2020-04-04 00:32:10 +0530

    Changed App test

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started again and again 1, edit <code>src/App.js</code> and save to reload.
+          To get started . Changed in feature, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
